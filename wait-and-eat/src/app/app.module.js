8098a5a9912562d437dcfe2d8cbe24f0caff201d
Randy Davis <randy_davis@outlook.com>
2016-02-01 23:51:00 -0600

    create main app module

---
+++ wait-and-eat/src/app/app.module.js
@@ -0,0 +1,14 @@
+/**
+ * Created by randy on 2/1/16.
+ */
+(function() {
+    'use strict';
+
+    angular.module('app', [
+        // Angular Modules
+        'ngRoute',
+
+        // 3rd Party Modules
+        'firebase'
+    ]);
+})();
