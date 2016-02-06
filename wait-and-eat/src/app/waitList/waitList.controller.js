3e089ae1913694a604d196da00384d87befa2344
Randy Davis <randy_davis@outlook.com>
2016-02-06 15:48:48 -0600

    finished writing a function for handling messaging

--- wait-and-eat/src/app/waitList/waitList.controller.js
+++ wait-and-eat/src/app/waitList/waitList.controller.js
@@ -10,12 +10,14 @@
     function WaitListCtrl ($firebaseArray) {
         var vm = this;
 
-        var fireParties = new Firebase('enter firebase url here');
+        var fireParties = new Firebase('https://waitandeat-v2-randy.firebaseio.com/parties');
+        var fireTextMessages = new Firebase('https://waitandeat-v2-randy.firebaseio.com/textMessages');
 
         vm.newParty = new Party();
         vm.parties = $firebaseArray(fireParties);
         vm.addParty = addParty;
         vm.removeParty = removeParty;
+        vm.sendTextMessage = sendTextMessage;
 
         function Party () {
             this.name = '';
@@ -32,6 +34,17 @@
 
         function removeParty (party) {
             vm.parties.$remove(party);
+        }
+
+        function sendTextMessage (party) {
+            var newTextMessage = {
+                phoneNumber: party.phone,
+                size: party.size,
+                name: party.name
+            };
+            fireTextMessages.push(newTextMessage);
+            party.notified = true;
+            vm.parties.$save(party);
         }
 
     }
