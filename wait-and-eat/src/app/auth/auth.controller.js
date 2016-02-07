488832ff553d70dd0348e858a91c8e2fbbd65008
Randy Davis <randy_davis@outlook.com>
2016-02-07 16:03:14 -0600

    finished the register function

--- wait-and-eat/src/app/auth/auth.controller.js
+++ wait-and-eat/src/app/auth/auth.controller.js
@@ -9,11 +9,18 @@
 
     function AuthCtrl ($firebaseAuth) {
         var vm = this;
+        var firebaseReference = new Firebase('https://waitandeat-v2-randy.firebaseio.com/');
+        var firebaseAuthObject = $firebaseAuth(firebaseReference);
 
+
+        vm.user = {
+            email: '',
+            password: ''
+        };
         vm.register = register;
 
         function register(user) {
-
+            return firebaseAuthObject.$createUser(user);
         }
 
 
